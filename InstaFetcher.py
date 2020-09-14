72c1e629c57d6654e7725b5f3e5d7f489a2bb6cd
Aswin Asok <aswinasokofficial@gmail.com>
2020-09-14 20:10:44 +0530

    Auto-Scroll Enabled

--- InstaFetcher.py
+++ InstaFetcher.py
@@ -27,113 +27,122 @@ time.sleep(6)
 
 for user in users:
 
-    print("\n---------Fetching The Followers List---------")
-    browser.get(f"https://www.instagram.com/{user}/")
+      print("\n---------Fetching The Followers List---------")
+      browser.get(f"https://www.instagram.com/{user}/")
 
-    posts, followers, following = browser.find_elements_by_class_name('g47SY')
-    print(f"\nTotal Posts: {posts.text}\nTotal Followers: {followers.text}\nTotal Followings: {following.text}\n")
+      posts, followers, following = browser.find_elements_by_class_name('g47SY')
+      print(f"\nTotal Posts: {posts.text}\nTotal Followers: {followers.text}\nTotal Followings: {following.text}\n")
 
-    lists = browser.find_elements_by_class_name('LH36I')
-    lists[1].click()
-
-
-    print("Sleep Started\n")
-    print("**Please Scroll Down The Entire List.\n")
-    t1  = int(input("Enter the Time Required for Scrolling(in Seconds) : "))
-    time.sleep(t1)
-    print("Sleep Over")
-
-    followerscountlist = browser.find_elements_by_class_name('wo9IH')
-
-    followersnamelist = browser.find_elements_by_class_name('Jv7Aj.MqpiF')
-
-    print("\nTotal Number of Followers Aquired : ",len(followersnamelist))
-
-    if len(followersnamelist)<int(followers.text):
-          print("\nComplete list of follwers has not been retrieved please increase the sleep duration")
-          print("Proceeding with Accuracy : ",(len(followersnamelist)/int(followers.text))*100.0)
-    else:
-          print("\nComplete List of Follwers has been Retrived\n")
+      lists = browser.find_elements_by_class_name('LH36I')
+      lists[1].click()
 
 
-    outF = open("followers.txt", "w")
-    print("\nWriting the Usernames of Follwers to a Text File\n")
+      print("Sleep Started\n")
+      print("**Please Wait.\n")
+      time.sleep(3)
+      scr1 = browser.find_element_by_xpath('/html/body/div[4]/div/div/div[2]')
 
-    i=0
-    while i<len(followersnamelist):
-         if i != "Verified":
-          outF.write("@"+str(followersnamelist[i].text))
-          outF.write("\n")
-          i=i+1
-    outF.close()
-    print("Writing Completed\n")
+      while not (str(len((browser.find_element_by_xpath("/html/body/div[4]/div/div/div[2]/ul/div")).find_elements_by_tag_name("li"))) == followers.text):
+            browser.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scr1)
 
-    i=0
-    while i<len(followersnamelist):
-          followersnamelist[i] = str(followersnamelist[i].text)
-          i=i+1
+
+      followerscountlist = browser.find_elements_by_class_name('wo9IH')
+
+      followersnamelist = browser.find_elements_by_class_name('Jv7Aj.MqpiF')
+
+      print("\nTotal Number of Followers Aquired : ",len(followersnamelist))
+
+      if len(followersnamelist)<int(followers.text):
+            print("\nComplete list of follwers has not been retrieved please increase the sleep duration")
+            print("Proceeding with Accuracy : ",(len(followersnamelist)/int(followers.text))*100.0)
+      else:
+            print("\nComplete List of Follwers has been Retrived\n")
+
+
+      outF = open("followers.txt", "w")
+      print("\nWriting the Usernames of Follwers to a Text File\n")
+
+      i=0
+      while i<len(followersnamelist):
+            if i != "Verified":
+                  outF.write("@"+str(followersnamelist[i].text))
+                  outF.write("\n")
+            i=i+1
+      outF.close()
+      print("Writing Completed\n")
+
+      i=0
+      while i<len(followersnamelist):
+            followersnamelist[i] = str(followersnamelist[i].text)
+            i=i+1
 
 print("\n------------Fetching the Followings List---------------")
 
 for user in users:
-    browser.get(f"https://www.instagram.com/{user}/")
+      browser.get(f"https://www.instagram.com/{user}/")
 
-    posts, followers, following = browser.find_elements_by_class_name('g47SY')
+      posts, followers, following = browser.find_elements_by_class_name('g47SY')
 
-    lists = browser.find_elements_by_class_name('LH36I')
-    lists[2].click()
+      lists = browser.find_elements_by_class_name('LH36I')
+      lists[2].click()
 
-    print("\nSleep Started\n")
-    print("**Please Scroll Down The Entire List.\n")
-    t2 = int(input("Enter the Time Required for Scrolling(in Seconds) : "))
-    time.sleep(t2)
-    print("Sleep Over\n")
+      print("Sleep Started\n")
+      print("**Please Wait.\n")
+      time.sleep(3)
+      scr1 = browser.find_element_by_xpath('/html/body/div[4]/div/div/div[2]')
 
-    followingcountlist = browser.find_elements_by_class_name('wo9IH')
-
-    followingnamelist = browser.find_elements_by_class_name('Jv7Aj.MqpiF')
-
-    print("Total Number of Followings Aquired : ",len(followingnamelist))
-
-    if len(followingnamelist)<int(following.text):
-          print("\nComplete list of follwings has not been retrieved please increase the sleep duration")
-          print("Proceeding with Accuracy : ",(len(followingnamelist)/int(following.text))*100.0)
-
-    else:
-          print("\nComplete List of Follwings has been Retrived\n")
+      while not (str(len((browser.find_element_by_xpath("/html/body/div[4]/div/div/div[2]/ul/div")).find_elements_by_tag_name("li"))) == following.text):
+            browser.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scr1)
 
 
-    outF = open("following.txt", "w")
-    print("\nWriting the Usernames of Follwings to a Text File\n")
-    i=0
-    while i<len(followingnamelist):
-         if i != "Verified":
-          outF.write("@"+str(followingnamelist[i].text))
-          outF.write("\n")
-          i=i+1
-    outF.close()
-    print("Writing Completed\n")
+      followingcountlist = browser.find_elements_by_class_name('wo9IH')
 
-    i=0
-    while i<len(followingnamelist):
-          followingnamelist[i] = str(followingnamelist[i].text)
-          i=i+1
+      followingnamelist = browser.find_elements_by_class_name('Jv7Aj.MqpiF')
+
+      print("Total Number of Followings Aquired : ",len(followingnamelist))
+
+      if len(followingnamelist)<int(following.text):
+            print("\nComplete list of follwings has not been retrieved please increase the sleep duration")
+            print("Proceeding with Accuracy : ",(len(followingnamelist)/int(following.text))*100.0)
+
+      else:
+            print("\nComplete List of Follwings has been Retrived\n")
+
+
+      outF = open("following.txt", "w")
+      print("\nWriting the Usernames of Follwings to a Text File\n")
+      i=0
+      while i<len(followingnamelist):
+            if i != "Verified":
+                  outF.write("@"+str(followingnamelist[i].text))
+                  outF.write("\n")
+            i=i+1
+      outF.close()
+      print("Writing Completed\n")
+
+      i=0
+      while i<len(followingnamelist):
+            followingnamelist[i] = str(followingnamelist[i].text)
+            i=i+1
 
 print("-----------Finding Non-Followers------------\n")
 
 def nonFollowers(followers, followings):
-     outF = open("nonFollowers.txt", "w")
-     print("Writing the list of non-followers to a Text File")
-     count = 0
-     for i in followings :
-         if i not in followers:
-              if i != "Verified":
-                    count+=1
-                    outF.write("@"+i)
-                    outF.write("\n")
-     print("\nNumber of Non-Followers : ",count)
-     print("\nWriting Completed")
-     return nonFollowers
+      outF = open("nonFollowers.txt", "w")
+      print("Writing the list of non-followers to a Text File")
+      count = 0
+      for i in followings :
+            if i not in followers:
+                  if i != "Verified":
+                        count+=1
+                        outF.write("@"+i)
+                        outF.write("\n")
+
+      print("\nNumber of Non-Followers : ",count)
+      print("\nWriting Completed")
+      return nonFollowers
+
+
 
 nonFollowers(followersnamelist, followingnamelist)
 
